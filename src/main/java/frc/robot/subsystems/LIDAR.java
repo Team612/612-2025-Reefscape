2b0c3f6597a3ebd8be366ceb90792b5c650cca42
achyutdipu <achyutdipu@gmail.com>
2025-02-01 10:31:15 -0500

    fixed some problems, see if it works. Only changes in LIDAR.java, nothing else in rest

--- src/main/java/frc/robot/subsystems/LIDAR.java
+++ src/main/java/frc/robot/subsystems/LIDAR.java
@@ -5,13 +5,28 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LIDAR extends SubsystemBase {
   /** Creates a new LIDAR. */
-  public LIDAR() {}
+  private static I2C m_i2c;
+  public LIDAR() {
+	  m_i2c = new I2C(I2C.Port.kMXP,0x62);
+	  initLidar();     	          	 
+  }
+  public void initLidar() {		
+    // nothing to do
+  }
+  public int getDistance() {
+    byte[] buffer = new byte[2];
+    m_i2c.write(0x00, 0x04);
+    Timer.delay(0.04);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+    if (!m_i2c.read(0x8f, 2, buffer)) {
+      System.out.println("Error reading LIDAR data");
+      return -1;
+    } 	
+    return ((buffer[0] & 0xFF) << 8) | (buffer[1] & 0xFF);
   }
 }
