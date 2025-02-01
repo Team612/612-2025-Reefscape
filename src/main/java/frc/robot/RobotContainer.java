a0f039e36a45531f0a293e949c48ddc541fc1d44
achyutdipu <achyutdipu@gmail.com>
2025-02-01 11:01:34 -0500

    Merged with Mecanum branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Mecanum m_drivetrain = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
--- src/main/java/frc/robot/controls/ControlMap.java
+++ src/main/java/frc/robot/controls/ControlMap.java
@@ -1,10 +1,17 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+package frc.robot.control;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-package frc.robot.controls;
-
-/** Add your docs here. */
-public class ControlMap {
+public class ControlMap{
+    private static int DRIVER_PORT = 0;
+    // private static int GUNNER_PORT = 1;
+    private static int DRIVER_PORT_BUTTONS = 2;
+    private static int GUNNER_PORT_BUTTONS = 3;
     
+    public static CommandXboxController driver_joystick = new CommandXboxController(DRIVER_PORT);
+    public static Joystick driver_buttons = new Joystick(DRIVER_PORT_BUTTONS);
+    public static Joystick gunner_buttons = new Joystick(GUNNER_PORT_BUTTONS);
+
+
 }
