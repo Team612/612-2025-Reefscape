b01d03f2ee1af0d11f281c0f68d8b7092f2793d3
achyutdipu <achyutdipu@gmail.com>
2025-02-06 17:20:58 -0500

    Added Arms subsystem

--- src/main/java/frc/robot/subsystems/Arms.java
+++ src/main/java/frc/robot/subsystems/Arms.java
@@ -1,44 +1,46 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+/***
+ * VERY IMPORTANT:
+ * The arms are controlled by a WINDOW motor.
+ * This means that there is no encodor for it.
+ * This will be controlled purely by the limit switches.
+ */
 package frc.robot.subsystems;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.LimitSwitchConfig;
-// import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.LimitSwitchConfig.Type;
+
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.LimitSwitchConfig;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
-public class Pivot extends SubsystemBase {
-  /** Creates a new Pivot. */
-  private final SparkMax m_pivot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arms extends SubsystemBase {
+  /** Creates a new Arms. */
+  private final SparkMax m_arms_motor;
   private SparkBaseConfig config;
   private ResetMode resetMode;
   private PersistMode persistMode;
   private LimitSwitchConfig limitSwitches;
-  public Pivot(SparkMax pivot) {
-    this.m_pivot = pivot;
+  public Arms(SparkMax arms_motor) {
+    this.m_arms_motor = arms_motor;
     config = new SparkMaxConfig();
     limitSwitches = new LimitSwitchConfig();
   }
 
-  public double getVelocity() {
-    return m_pivot.getAbsoluteEncoder().getVelocity();
-  }
-
   public boolean getForwardLimitSwitchPressed() {
-    return m_pivot.getForwardLimitSwitch().isPressed();
+    return m_arms_motor.getForwardLimitSwitch().isPressed();
   }
   public boolean getReverseLimitSwitchPressed() {
-    return m_pivot.getReverseLimitSwitch().isPressed();
+    return m_arms_motor.getReverseLimitSwitch().isPressed();
   }
   private void setConfig() {
-    m_pivot.configure(config, resetMode, persistMode);
+    m_arms_motor.configure(config, resetMode, persistMode);
   }
   public void setForwardLimitSwitch(boolean enabled) { // disables motor shutdown if limitswitch is pressed (false enables)
     config.apply(limitSwitches.forwardLimitSwitchEnabled(enabled));
@@ -57,7 +59,7 @@ public class Pivot extends SubsystemBase {
     this.setConfig();
   }
   public void setVelocity(double speed) {
-    m_pivot.set(speed);
+    m_arms_motor.set(speed);
   }
   public void setResetMode(boolean safe) {
     resetMode = safe ? ResetMode.kResetSafeParameters : ResetMode.kNoResetSafeParameters;
@@ -67,12 +69,12 @@ public class Pivot extends SubsystemBase {
     persistMode = parameters ? PersistMode.kPersistParameters : PersistMode.kNoPersistParameters;
     this.setConfig();
   }
+
   @Override
   public void periodic() {
     this.setPersistMode(true);
-    SmartDashboard.putNumber("Pivot Velocity", this.getVelocity());
-    SmartDashboard.putBoolean("Pivot Forward Switch", this.getForwardLimitSwitchPressed());
-    SmartDashboard.putBoolean("Pivot Reverse Switch", this.getReverseLimitSwitchPressed());
+    SmartDashboard.putBoolean("Arms Forward Switch", this.getForwardLimitSwitchPressed());
+    SmartDashboard.putBoolean("Arms Reverse Switch", this.getReverseLimitSwitchPressed());
     // This method will be called once per scheduler run
   }
 }
