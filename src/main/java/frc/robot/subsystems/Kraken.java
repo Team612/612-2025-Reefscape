cc35e4b54a05e5dbf7fdac1c7fc64d1dd8dad402
achyutdipu <achyutdipu@gmail.com>
2025-02-07 17:06:01 -0500

    kraken subsystem

--- src/main/java/frc/robot/subsystems/Kraken.java
+++ src/main/java/frc/robot/subsystems/Kraken.java
@@ -4,14 +4,46 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Kraken extends SubsystemBase {
   /** Creates a new Kraken. */
-  public Kraken() {}
-
+  private TalonFX kraken;
+  public Kraken(TalonFX kraken) {
+    this.kraken = kraken;
+  }
+  public void setVelocity(double speed) {
+    kraken.set(speed);
+  }
+  public void setVoltage(double volts) {
+    kraken.setVoltage(volts);
+  }
+  public void stop() {
+    kraken.stopMotor();
+  }
+  public double getVelocity() {
+    return kraken.get();
+  }
+  public boolean motorAlive() {
+    return kraken.isAlive();
+  }
+  public boolean safetyEnabled() {
+    return kraken.isSafetyEnabled();
+  }
+  public void setSafety(boolean enabled) {
+    kraken.setSafetyEnabled(enabled);
+  }
+  public double getVoltage() {
+    return kraken.getMotorVoltage().getValueAsDouble();
+  }
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Kraken velocity", this.getVelocity());
+    SmartDashboard.putBoolean("Kraken alive", this.motorAlive());
+    SmartDashboard.putBoolean("Kraken Safety", this.safetyEnabled());
     // This method will be called once per scheduler run
   }
 }
