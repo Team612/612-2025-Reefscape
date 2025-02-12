06468be5814e424c91c5e14da92e235d31aa4c3e
VictorMoldoveanu <victormoldoveanu07@gmail.com>
2025-02-12 16:09:31 -0500

    I got no clue why it deleated everything the first time but here is the same thing as before

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,22 +1,38 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  public Robot() {}
+ //constants
+  private static final int constantXboxControllerPortNumber = 0;
+  private static final int SPARK_FL = 2;
+  private static final int SPARK_BR = 4;
+  private static final int SPARK_BL = 3;
+  private static final int SPARK_FR = 1;
+  private static final int PigeonID = 0;
+  private static final Double DEADZONE = 0.05;
+
+  // instantiate things here
+  private XboxController controller = new XboxController(constantXboxControllerPortNumber);
+  private final static SparkMax spark_fl = new SparkMax(SPARK_FL,MotorType.kBrushless);
+  private final static SparkMax spark_fr = new SparkMax(SPARK_FR,MotorType.kBrushless);
+  private final static SparkMax spark_bl = new SparkMax(SPARK_BL,MotorType.kBrushless);
+  private final static SparkMax spark_br = new SparkMax(SPARK_BR,MotorType.kBrushless);
+  private Pigeon2 gyro = new Pigeon2(PigeonID);
+  private static MecanumDrive mech = new MecanumDrive(spark_fl, spark_bl, spark_fr, spark_br);
+  
+  // aplies deadband so the robot does not drift when you let go of the controller.
+  public Robot() {
+    mech.setDeadband(DEADZONE);
+  }
 
   @Override
   public void robotPeriodic() {}
@@ -31,7 +47,20 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    //resets the gyroscope to zero for field oriented if needed
+    if (controller.getXButtonPressed()){
+      gyro.reset();
+    }
+    
+    //gets desired percent movement
+    double x = -controller.getRawAxis(0);
+    double y = -controller.getRawAxis(1);
+    double zRot = controller.getRawAxis(4);
+
+    //automatically calculates all wheel movements and aplies them to achieve holonomic drive
+    mech.driveCartesian(y, x, zRot, gyro.getRotation2d());
+  }
 
   @Override
   public void disabledInit() {}
