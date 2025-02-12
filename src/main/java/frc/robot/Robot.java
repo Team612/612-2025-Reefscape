1158397607a04a8d04b987b91d6458e0ad37d918
VictorMoldoveanu <victormoldoveanu07@gmail.com>
2025-02-12 16:20:02 -0500

    copy and pasted all of the code from my other folder so it will be in github

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,95 +1,100 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+  // constants
+  private int cim1ID = 15;
+  private int cim2ID = 2;
+  private int cim3ID = 11;
+  private int cim4ID = 50;
+  private int neo1ID = 1;
+  private int neo2ID = 10;
+  private int controllerPort = 0;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+  //instantiation
+  private TalonSRX cim1 = new TalonSRX(cim1ID);
+  private TalonSRX cim2 = new TalonSRX(cim2ID);
+  private TalonSRX cim3 = new TalonSRX(cim3ID);
+  private TalonSRX cim4 = new TalonSRX(cim4ID);
+  private SparkMax neo1 = new SparkMax(neo1ID, MotorType.kBrushless);
+  private SparkMax neo2 = new SparkMax(neo2ID, MotorType.kBrushless);
+
+  private CommandXboxController controller = new CommandXboxController(controllerPort);
+
+  public Robot() {
+    cim1.setInverted(true);
+    cim2.setInverted(true);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
+  public void resetMotors(){
+    cim1.set(TalonSRXControlMode.PercentOutput,0);
+    cim2.set(TalonSRXControlMode.PercentOutput,0);
+    cim3.set(TalonSRXControlMode.PercentOutput,0);
+    cim4.set(TalonSRXControlMode.PercentOutput,0);
+    neo1.set(0);
+    neo2.set(0);
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
+  // robot lifecycle
   @Override
-  public void disabledInit() {}
+  public void robotPeriodic() {}
 
   @Override
-  public void disabledPeriodic() {}
+  public void autonomousInit() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    resetMotors();
   }
 
-  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double speed = -controller.getRightY();
+    double turn = controller.getRightX();
+    double deflator = 1;
+    if (Math.abs(speed) + Math.abs(turn) > 1)
+      deflator = 1/(Math.abs(speed) + Math.abs(turn));
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
+    cim1.set(TalonSRXControlMode.PercentOutput,(speed + turn) * deflator);
+    cim2.set(TalonSRXControlMode.PercentOutput,(speed + turn) * deflator);
+    cim3.set(TalonSRXControlMode.PercentOutput,(speed - turn) * deflator);
+    cim4.set(TalonSRXControlMode.PercentOutput,(speed - turn) * deflator);
+
+    double intakeSpeed = controller.getLeftTriggerAxis();
+    System.out.println(controller.getLeftTriggerAxis());
+    neo1.set(-intakeSpeed);
+
+    double outtakeSpeed = controller.getRightTriggerAxis();
+    System.out.println(controller.getRightTriggerAxis());
+    neo2.set(-outtakeSpeed);
   }
 
-  /** This function is called periodically during test mode. */
+  @Override
+  public void disabledInit() {
+    resetMotors();
+  }
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
   @Override
   public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
