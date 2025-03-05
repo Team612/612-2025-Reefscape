d8c3a65f571d2dedc5a9eea55ca521d8c2f5c861
VictorMoldoveanu <victormoldoveanu07@gmail.com>
2025-03-05 15:52:30 -0500

    added driver and gunner controls

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,7 @@ package frc.robot;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Servo;
@@ -12,35 +13,44 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
   //constants
-  private static final int constantXboxControllerPortNumber = 0;
+  private static final int driverPort = 0;
+  private static final int gunnerPort = 1;
   private static final int SPARK_FL = 2;
   private static final int SPARK_BR = 4;
   private static final int SPARK_BL = 3;
   private static final int SPARK_FR = 1;
   private static final int PigeonID = 0;
-  private static final Double DEADZONE = 0.05;
+  private static final double DEADZONE = 0.05;
   private static final int IntakePivotID = 6;
+  private static final double IntakePivotSpeed = 0.5;
   private static final int IntakeBagID = 7;
-  private static final double IntakePivotSpeed = 0.3;
-  private static final double IntakeBagSpeed = 0.3;
   private static final int ElevatorID = 5;
   private static final int ServoID = 0;
   private static final int ClimbPivotID = 8;
   private static final double ClimbPivotSpeed = 0.1;
+<<<<<<< Updated upstream
   private static final double Elevatorkp = 1;
   private static final double maxAutoElevatorSpeed = 0.5;
-  private static final double coralStationPosition = 0.316;
+  private static final double coralStationPosition = -0.316;
+  private static final double L2Position = -0.236;
+  private static final double L3Position = -0.654;
+=======
+  private static final double Elevatorkp = 1.0;
+  private static final double maxAutoElevatorSpeed = 0.5;
+  private static final double coralStation = 0.316;
   private static final double L2Position = 0.236;
   private static final double L3Position = 0.654;
+>>>>>>> Stashed changes
 
   // instantiate things here
-  private XboxController controller = new XboxController(constantXboxControllerPortNumber);
+  private XboxController driveController = new XboxController(driverPort);
+  private XboxController gunController = new XboxController(gunnerPort);
   private final static SparkMax spark_fl = new SparkMax(SPARK_FL,MotorType.kBrushless);
   private final static SparkMax spark_fr = new SparkMax(SPARK_FR,MotorType.kBrushless);
   private final static SparkMax spark_bl = new SparkMax(SPARK_BL,MotorType.kBrushless);
   private final static SparkMax spark_br = new SparkMax(SPARK_BR,MotorType.kBrushless);
   private final static SparkMax intakePivot = new SparkMax(IntakePivotID,MotorType.kBrushless);
-  private final static SparkMax intakeBagSpeed = new SparkMax(IntakeBagID,MotorType.kBrushless);
+  private final static SparkMax intakeBag = new SparkMax(IntakeBagID,MotorType.kBrushless);
   private final static SparkMax elevator = new SparkMax(ElevatorID,MotorType.kBrushless);
   private final static Servo servo = new Servo(ServoID);
   private final static SparkMax climbPivot = new SparkMax(ClimbPivotID,MotorType.kBrushless);
@@ -54,6 +64,12 @@ public class Robot extends TimedRobot {
   public Robot() {
     mech.setDeadband(DEADZONE);
     elevator.setInverted(true);
+<<<<<<< Updated upstream
+=======
+    
+    if (elevator.getForwardLimitSwitch().isPressed())
+      elevator.getEncoder().setPosition(0);
+>>>>>>> Stashed changes
   }
 
   @Override
@@ -63,7 +79,7 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("spark_bl Speed", spark_bl.get());
     SmartDashboard.putNumber("spark_br Speed", spark_br.get());
     SmartDashboard.putNumber("intakePivot Speed", intakePivot.get());
-    SmartDashboard.putNumber("intakeBagSpeed Speed", intakeBagSpeed.get());
+    SmartDashboard.putNumber("intakeBagSpeed Speed", intakeBag.get());
     SmartDashboard.putNumber("elevator Speed", elevator.get());
     SmartDashboard.putNumber("climbPivot Speed", climbPivot.get());
     SmartDashboard.putNumber("servo Position", servo.get());
@@ -88,35 +104,20 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     //resets the gyroscope to zero for field oriented if needed
-    if (controller.getRightBumperButton())
+    if (driveController.getRightBumperButton())
       gyro.reset();
     
     //gets desired percent movement
-    double x = -controller.getRawAxis(0);
-    double y = -controller.getRawAxis(1);
-    double zRot = controller.getRawAxis(4);
+    double x = -driveController.getRawAxis(0);
+    double y = -driveController.getRawAxis(1);
+    double zRot = driveController.getRawAxis(4);
 
     //automatically calculates all wheel movements and aplies them to achieve holonomic drive
     mech.driveCartesian(y, x, zRot, gyro.getRotation2d());
 
-    // controls pivot movement using X and B buttons
-    if (controller.getXButton())
-      intakePivot.set(IntakePivotSpeed);
-    else if (controller.getBButton())
-      intakePivot.set(-IntakePivotSpeed);
-    else
-      intakePivot.set(0);
-
-    // controls intake bag movement using Y and A buttons
-    if (controller.getYButton())
-      intakeBagSpeed.set(IntakeBagSpeed);
-    else if (controller.getAButton())
-      intakeBagSpeed.set(-IntakeBagSpeed);
-    else
-      intakeBagSpeed.set(0);
-
-    // controls elevator speed using both triggers
+    // controls elevator using gunner buttons
     // and adds pid control
+<<<<<<< Updated upstream
     if ((controller.getRightTriggerAxis() != 0) || (controller.getLeftTriggerAxis() != 0))
       Lposition = -1;
     if (controller.getRawButtonPressed(7))
@@ -128,22 +129,62 @@ public class Robot extends TimedRobot {
     
     double elevatorSpeed = 0.0;
     if (Lposition == -1){
-      elevatorSpeed = controller.getRightTriggerAxis()-controller.getLeftTriggerAxis();
+      elevatorSpeed = controller.getLeftTriggerAxis()-controller.getRightTriggerAxis();
       elevator.set(elevatorSpeed);
     }
     else{
       if (Lposition == 1)
-        elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), coralStationPosition);
+        elevatorSpeed = elevatorController.calculate(elevator.getEncoder().getPosition(), coralStationPosition);
       else if (Lposition == 2)
-        elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), L2Position);
+        elevatorSpeed = elevatorController.calculate(elevator.getEncoder().getPosition(), L2Position);
       else if (Lposition == 3)
-        elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), L3Position);
+        elevatorSpeed = elevatorController.calculate(elevator.getEncoder().getPosition(), L3Position);
       if (elevatorSpeed > maxAutoElevatorSpeed)
         elevatorSpeed = maxAutoElevatorSpeed;
       if (elevatorSpeed < -maxAutoElevatorSpeed)
         elevatorSpeed = -maxAutoElevatorSpeed;
-      elevator.set(elevatorSpeed);
+=======
+    if (gunController.getBButton())
+      Lposition = 0;
+    if (gunController.getXButton())
+      Lposition = 1;
+    if (gunController.getAButton())
+      Lposition = 2;
+    if (gunController.getYButton())
+      Lposition = 3;
+    
+    double elevatorSpeed = 0;
+    if (Lposition == 0){
+      elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), 0);
+      elevatorSpeed = applyMin(elevatorSpeed);
+      intakePivot.set(IntakePivotSpeed);
+      if (intakePivot.getForwardLimitSwitch().isPressed())
+        elevator.set(elevatorSpeed);
+      else
+        elevator.set(0);
     }
+    if (Lposition == 1){
+      elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), coralStation);
+      elevatorSpeed = applyMin(elevatorSpeed);
+>>>>>>> Stashed changes
+      elevator.set(elevatorSpeed);
+      intakePivot.set(-IntakePivotSpeed);
+    }
+    if (Lposition == 2){
+      elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), L2Position);
+      elevatorSpeed = applyMin(elevatorSpeed);
+      elevator.set(elevatorSpeed);
+      intakePivot.set(IntakePivotSpeed);
+    }
+    if (Lposition == 3){
+      elevatorSpeed = -elevatorController.calculate(elevator.getEncoder().getPosition(), L3Position);
+      elevatorSpeed = applyMin(elevatorSpeed);
+      elevator.set(elevatorSpeed);
+      intakePivot.set(IntakePivotSpeed);
+    }
+
+    // controls intake bag motor with gunner variable inputs
+    intakeBag.set(gunController.getRightTriggerAxis()-gunController.getLeftTriggerAxis());
 
     // controls climb using the POV buttons
     // if (controller.getPOV() == 0)
@@ -158,6 +199,17 @@ public class Robot extends TimedRobot {
     //   servo.set(0);
     // else
     //   servo.set(0.5);
+<<<<<<< Updated upstream
+=======
+  }
+
+  public static double applyMin(double elevatorSpeed){
+    if (elevatorSpeed > maxAutoElevatorSpeed)
+      elevatorSpeed = maxAutoElevatorSpeed;
+    if (elevatorSpeed < -maxAutoElevatorSpeed)
+      elevatorSpeed = -maxAutoElevatorSpeed;
+    return elevatorSpeed;
+>>>>>>> Stashed changes
   }
 
   @Override
@@ -176,5 +228,5 @@ public class Robot extends TimedRobot {
   public void simulationInit() {}
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {} 
 }
