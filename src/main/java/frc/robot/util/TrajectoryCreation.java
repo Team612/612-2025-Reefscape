1207bdd83f0e2077b5ca7fa148181499b46859f2
achyutdipu <achyutdipu@gmail.com>
2025-03-10 20:45:06 -0400

    changing distance from apriltag in midpoint from 2m to 1m

--- src/main/java/frc/robot/util/TrajectoryCreation.java
+++ src/main/java/frc/robot/util/TrajectoryCreation.java
@@ -171,8 +171,8 @@ public class TrajectoryCreation {
         Rotation2d tagAngle = vision.return_tag_pose(result.getBestTarget().getFiducialId()).getRotation().toRotation2d().rotateBy(new Rotation2d(Units.degreesToRadians(180)));
         //final pose is the tag pose transformed by a certain distance RELATIVE to the tag (with robot coordinates)
         //note that the heading should be the tag angle so it faces outward. Putting the tag angle as the heading results in trajectory heading towards the tag, not infront
-        Pose2d midPose1 = new Pose2d(tagX, tagY, tagAngle).transformBy(new Transform2d(new Translation2d(displacementX-2,displacementY),new Rotation2d()));
-        Pose2d midPose2 = new Pose2d(tagX, tagY, tagAngle).transformBy(new Transform2d(new Translation2d(displacementX-2,displacementY+Units.inchesToMeters(12.947811)),new Rotation2d()));
+        Pose2d midPose1 = new Pose2d(tagX, tagY, tagAngle).transformBy(new Transform2d(new Translation2d(displacementX-1,displacementY),new Rotation2d()));
+        Pose2d midPose2 = new Pose2d(tagX, tagY, tagAngle).transformBy(new Transform2d(new Translation2d(displacementX-1,displacementY+Units.inchesToMeters(12.947811)),new Rotation2d()));
         Pose2d finalPose = new Pose2d(tagX, tagY, tagAngle).transformBy(new Transform2d(new Translation2d(displacementX,displacementY),new Rotation2d()));
         
         List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(
