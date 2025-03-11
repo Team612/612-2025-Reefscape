b343444d322b31acda2397e0dc8cce15bf02c54a
SrikarAkundi <akundisrikar@gmail.com>
2025-03-11 19:45:33 -0400

    color sensor

--- src/main/java/frc/robot/subsystems/Sensor.java
+++ src/main/java/frc/robot/subsystems/Sensor.java
@@ -6,22 +6,33 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Sensor extends SubsystemBase {
   /** Creates a new LIDAR. */
-  AnalogInput analog;
-
+  private static I2C m_i2c;
   public Sensor() {
-	    analog = new AnalogInput(3);
+	  m_i2c = new I2C(I2C.Port.kMXP,0x52);
+	  initLidar();     	          	 
   }
-
   public void initLidar() {		
     // nothing to do
   }
   public int getDistance() {
-    return analog.getValue();
+    byte[] buffer = new byte[2];
+    m_i2c.write(0x00, 0x04);
+    Timer.delay(0.04);
+
+    if (!m_i2c.read(0x8f, 2, buffer)) {
+      System.out.println("Error reading Color data");
+      return -1;
+    } 	
+    return ((buffer[0] & 0xFF) << 8) | (buffer[1] & 0xFF);
+  }
+  @Override
+  public void setDefaultCommand(Command defaultCommand) {
+      // TODO Auto-generated method stub
+      super.setDefaultCommand(defaultCommand);
   }
 }
