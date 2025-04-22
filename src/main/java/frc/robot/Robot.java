eff7eebb22fea68dca86c60ced0bd338c7935265
VictorMoldoveanu <victormoldoveanu07@gmail.com>
2025-04-22 09:57:00 -0400

    added all of my optical sensor code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,68 +1,184 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
-import java.net.http.WebSocket;
-import java.nio.file.FileSystem;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.net.WebServer;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.util.Telemetry;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
-  private Telemetry m_telemetry;
+  //constants
+  private int constantXboxControllerPortNumber = 0;
 
-  private final RobotContainer m_robotContainer;
+  // instantiate things here
+  private XboxController controller = new XboxController(constantXboxControllerPortNumber);
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
-    m_telemetry = new Telemetry();
+  // object that interacts with the I2C port to which the sensor is connected to
+  private I2C sensor = new I2C(I2C.Port.kOnboard,0x17);
+
+  // Read a 16-bit signed value from the sensor
+  private int read16Bit(int register) {
+    byte[] buffer = new byte[2];
+    sensor.read(register, 2, buffer);
+    // return (buffer[1] << 8) | (buffer[0]);
+    return ((buffer[1] & 0xFF) << 8) | (buffer[0] & 0xFF); 
   }
 
-  public void robotInit() {
-    WebServer.start(5800, Filesystem.getDeployDirectory().getPath()); //for elastic
-    m_telemetry.initData();
-    // if (!Preferences.containsKey("Pay Speed")){
-    //   Preferences.setDouble("Pay Speed", Constants.ElevatorConstants.payloadspeed);
-    // }
-    // if (!Preferences.containsKey("Pivot Speed")){
-    //   Preferences.setDouble("Pivot Speed", Constants.IntakeConstants.pivotspeed);
-    // }
-    // if (!Preferences.containsKey("Bag Speed")){
-    //   Preferences.setDouble("Bag Speed", Constants.IntakeConstants.bagspeed);
-    // }
-    // if (!Preferences.containsKey("Intake Pivot kP")){
-    //   Preferences.setDouble("Intake Pivot kP", Constants.IntakeConstants.kP);
-    // }
-    // if (!Preferences.containsKey("Intake Pivot kI")){
-    //   Preferences.setDouble("Intake Pivot kI", Constants.IntakeConstants.kI);
-    // }
-    // if (!Preferences.containsKey("Intake Pivot kD")){
-    //   Preferences.setDouble("Intake Pivot kD", Constants.IntakeConstants.kD);
-    // }
-    // if (!Preferences.containsKey("Elevator kP")){
-    //   Preferences.setDouble("Elevator kP", Constants.ElevatorConstants.kP);
-    // }
-    // if (!Preferences.containsKey("Elevator kI")){
-    //   Preferences.setDouble("Elevator kI", Constants.ElevatorConstants.kI);
-    // }
-    // if (!Preferences.containsKey("Elevator kD")){
-    //   Preferences.setDouble("Elevator kD", Constants.ElevatorConstants.kD);
-    // }
+  // the low byte for the heading returns garbage data for some reason so we are only reading the high byte
+  public int getHeading(){
+    byte[] buffer = new byte[2];
+    sensor.read(0x25,2,buffer);
+    return ((buffer[0] & 0xFF));
   }
+
+  // returns the binary data from a register in 1s and 0s
+  public String debug(int register){
+    byte[] buffer = new byte[2];
+    sensor.read(register, 2, buffer);
+
+    String binaryStringIndex0 = String.format("%8s", Integer.toBinaryString(buffer[0] & 0xFF)).replace(' ', '0');
+    String binaryStringIndex1 = String.format("%8s", Integer.toBinaryString(buffer[1] & 0xFF)).replace(' ', '0');
+    
+    return (binaryStringIndex0 + " " + binaryStringIndex1);
+  }
+
+  // Get X and Y movement deltas
+  public int getDeltaX() {
+    return read16Bit(0x20);
+  }
+  public int getDeltaY() {
+    return read16Bit(0x22);
+  }
+
+  public void reset(){
+    sensor.write(0x07, 0x01);
+    try {
+      // three second delay to attempt to reduce errors
+      // reduce or remove this if you are tired of waiting
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      System.out.println("Error Pausing");
+    }
+  }
+
+  public void setLinearScalar(double scalar) {
+    // Check if the scalar is out of bounds
+    if (scalar < 0.872 || scalar > 1.127)
+      return;
+
+    // Convert to integer, multiples of 0.1% (+0.5 to round instead of truncate)
+    byte rawScalar = (byte) ((scalar - 1.0) * 1000 + 0.5);
+
+    sensor.write(0x04, rawScalar);
+  }
+
+  public void setAngularScalar(double scalar) {
+    // Check if the scalar is out of bounds
+    if (scalar < 0.872 || scalar > 1.127)
+        return;
+
+    // Convert to integer, multiples of 0.1% (+0.5 to round instead of truncate)
+    byte rawScalar = (byte) ((scalar - 1.0) * 1000 + 0.5);
+
+    sensor.write(0x05, rawScalar);
+  }
+
+  public boolean calibrateImu(int numSamples, boolean waitUntilDone) {
+    // Check if the number of samples is out of bounds
+    if (numSamples < 1 || numSamples > 255)
+        return false;
+
+    // Write the number of samples to the device
+    sensor.write(0x06, numSamples);
+
+    // Wait 1 sample period (2.4ms) to ensure the register updates
+    try {
+        Thread.sleep(3);
+    } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        return false;
+    }
+
+    // Do we need to wait until the calibration finishes?
+    if (!waitUntilDone)
+      return true;
+
+    // Wait for the calibration to finish, which is indicated by the IMU
+    // calibration register reading zero, or until we reach the maximum number
+    // of read attempts
+    for (int numAttempts = numSamples; numAttempts > 0; numAttempts--) {
+      // Read the gryo calibration register value
+      byte[] buffer = new byte[2];
+      sensor.read(0x06, 2, buffer);
+
+      byte calibrationValue = buffer[0];
+      // byte calibrationValue = buffer[1];
+
+      // Check if calibration is done
+      if (calibrationValue == 0)
+          return true;
+
+      // Give a short delay between reads. As of firmware v1.0, samples take
+      // 2.4ms each, so 3ms should guarantee the next sample is done. This
+      // also ensures the max attempts is not exceeded in normal operation
+      try {
+          Thread.sleep(3);
+      } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+          return false;
+      }
+    }
+
+    // Max number of attempts reached, calibration failed
+    return false;
+  }
+
+  public Robot() {}
 
   @Override
   public void robotPeriodic() {
-    m_telemetry.updateData();
-    CommandScheduler.getInstance().run();
+    int deltaX = getDeltaX();
+    int deltaY = getDeltaY();
+    int heading = getHeading();
+    double actualX = deltaX * 0.0003;
+    double actualY = deltaY * 0.0003;
+    double actualDegrees = heading * 360 / 256;
+
+    SmartDashboard.putNumber("X", actualX);
+    SmartDashboard.putNumber("Y", actualY);
+    SmartDashboard.putNumber("Heading", actualDegrees);
+
+    SmartDashboard.putString("register0x20", debug(0x20));
+    SmartDashboard.putString("register0x21", debug(0x21));
+    SmartDashboard.putString("register0x22", debug(0x22));
+    SmartDashboard.putString("register0x23", debug(0x23));
+    SmartDashboard.putString("register0x24", debug(0x24));
+    SmartDashboard.putString("register0x25", debug(0x25));
+  }
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (controller.getYButtonPressed()){
+      reset();
+    }
+    if (controller.getBButtonPressed()){
+      calibrateImu(254,true);
+    }
+    if (controller.getAButtonPressed()){
+      setLinearScalar(1.05);
+    }
+    if (controller.getXButtonPressed()){
+      setAngularScalar(0.9897222);
+    }
   }
 
   @Override
@@ -72,45 +188,14 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
-
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+  public void testInit() {}
 
   @Override
   public void testPeriodic() {}
 
   @Override
-  public void testExit() {}
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
 }
