ea88aec0802629b2da333c4cc21f60da3081c6a8
VictorMoldoveanu <victormoldoveanu07@gmail.com>
2025-12-12 00:27:47 -0500

    made a new branch

---
+++ src/main/java/frc/robot/commands/ResetEncoders.java
@@ -0,0 +1,19 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Swerve;
+
+public class ResetEncoders extends InstantCommand {
+  Swerve m_swerve;
+
+  public ResetEncoders() {
+    this.m_swerve = m_swerve;
+    addRequirements(m_swerve);
+  }
+
+  @Override
+  public void initialize() {
+    m_swerve.setPose(new Pose2d());
+  }
+}
